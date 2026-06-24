2cd85818ad2763c4a12b70206d154bbb34988091
whiteduke <dmitro.yurkov.23@gmail.com>
2026-06-24 20:30:32 +0300

    fix(subs): atomically write

--- internal/subscription/subscription.go
+++ internal/subscription/subscription.go
@@ -388,5 +388,32 @@ func SaveAll(subs []Subscription) error {
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(p, data, 0600)
+	return atomicwrite(p, data, 0600)
+}
+
+// atomicwrite lands data 
+func atomicwrite(path string, data []byte, perm os.FileMode) error {
+	tmp, err := os.CreateTemp(filepath.Dir(path), ".subs-*.tmp")
+	if err != nil {
+		return err
+	}
+	tmpname := tmp.Name()
+	defer os.Remove(tmpname)
+
+	if _, err := tmp.Write(data); err != nil {
+		tmp.Close()
+		return err
+	}
+	if err := tmp.Chmod(perm); err != nil {
+		tmp.Close()
+		return err
+	}
+	if err := tmp.Sync(); err != nil {
+		tmp.Close()
+		return err
+	}
+	if err := tmp.Close(); err != nil {
+		return err
+	}
+	return os.Rename(tmpname, path)
 }
